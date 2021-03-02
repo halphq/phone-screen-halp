be131c4af705dd68044798e8de4c09a750297ac8
Komran Rashidov <komran.rashidov@gmail.com>
2021-03-02 10:59:38 -0700

    First commit of skeleton

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # phone-screen-halp
+
+Hello & welcome to the Halp Phone Screen repository.
+
+If you don't have node installed on your local machine, please use the HackerRank instead
---
+++ main.js
@@ -0,0 +1,3 @@
+// https://alexwohlbruck.github.io/cat-facts/docs/
+// available libraries: lodash, underscore, axios, node-fetch, use npm install to install more
+console.log("hello world!");
